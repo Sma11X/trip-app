6cbc28913f41d3113e94f2441f907502ede619dc
FXX <540351143@qq.com>
2022-08-25 01:36:47 +0800

    feat: city page

--- src/router/index.js
+++ src/router/index.js
@@ -25,7 +25,7 @@ const router = createRouter({
     },
     {
       path: "/city",
-      component: () => import("@/views/city/city.vue"),
+      component: () => import("@/views/city/city.vue")
       // meta: {
       //   hideTabBar: true
       // }
--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -1,6 +1,9 @@
 <script setup>
+import { computed } from '@vue/reactivity'
+import { storeToRefs } from 'pinia'
 import { ref } from 'vue'
-import { useRouter } from 'vue-router';
+import { useRouter } from 'vue-router'
+import useCityStore from '../../stores/modules/city'
 
 const router = useRouter()
 
@@ -10,24 +13,60 @@ const cancelClick = () => {
 }
 
 const tabActive = ref()
+
+// store中获取数据
+const cityStore = useCityStore()
+cityStore.fetchAllCitiesData()
+const { allCities } = storeToRefs(cityStore)
+
+// 目的是为了获取其中一组数据并展示
+// 1.获取正确的key
+// 2.根据key获取数据，但是这个数据不是响应式的，需要用computed进行包裹
+const currentGroup = computed(() => allCities.value[tabActive.value])
+// console.log(cityStore)
+
 </script>
 
 <template>
   <div class="top-page">
-    <van-search
-      v-model="searchValue"
-      placeholder="城市/区域/位置"
-      shape="round"
-      show-action
-      @cancel="cancelClick"
-    />
-    <van-tabs v-model:active="tabActive" color="#ff9854">
-      <van-tab title="国内·港澳台"></van-tab>
-      <van-tab title="海外"></van-tab>
-    </van-tabs>
+    <div class="top">
+      <van-search
+        v-model="searchValue"
+        placeholder="城市/区域/位置"
+        shape="round"
+        show-action
+        @cancel="cancelClick"
+      />
+      <van-tabs v-model:active="tabActive" color="#ff9854" line-height="3">
+        <template v-for="(value, key, index) in allCities" :key="key">
+          <van-tab :title="value.title" :name="key"></van-tab>
+        </template>
+      </van-tabs>
+    </div>
+    <div class="content">
+      <template v-for="item in currentGroup?.cities">
+        <div>{{item}}</div>
+      </template>
+    </div>
   </div>
 </template>
 
 <style lang="less" scoped>
-
+  .city {
+    // top固定定位
+    // .top {
+    //   position: fixed;
+    //   top: 0;
+    //   left: 0;
+    //   right: 0;
+    // }
+    // .content {
+    //   padding-top: 98px;
+    // }
+    // 局部滚动
+    .content {
+      height: calc(100vh - 98px);
+      overflow-y: auto;
+    }
+  }
 </style>
